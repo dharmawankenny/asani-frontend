1715d0657668d3da3513ab7fa9232e43ef223e7d
Muhamad Septian <muhamad.septian@asani.co.id>
2018-11-06 19:01:48 +0700

    Minor Bug

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // TODO change PREFIX_API_URL to development server
-let PREFIX_API_URL = 'https://asani-backend-app.herokuapp.com';
+let PREFIX_API_URL = 'http://localhost:8000';
 // let PREFIX_API_URL = 'http://localhost:8080';
 
 if (process.env.NODE_ENV === 'production') {
