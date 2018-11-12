cfd793371ada5dd4f00a376b0222df4c08019b06
Muhamad Septian <muhamad.septian@asani.co.id>
2018-11-12 16:59:22 +0700

    New API

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 // TODO change PREFIX_API_URL to development server
-let PREFIX_API_URL = 'https://asani-backend-app.herokuapp.com';
+let PREFIX_API_URL = 'https://api-prod.asani.net';
 // let PREFIX_API_URL = 'http://localhost:8080';
 
 if (process.env.NODE_ENV === 'production') {
-  PREFIX_API_URL = 'https://asani-backend-app.herokuapp.com';
+  PREFIX_API_URL = 'https://api-prod.asani.net';
 }
 
 export const api = axios.create({
