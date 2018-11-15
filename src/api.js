f3ac184e994017429fd7179ef9e6e0f16bbbb90b
Muhamad Septian <muhamad.septian@asani.co.id>
2018-11-15 17:25:38 +0700

    URL API Staging

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // TODO change PREFIX_API_URL to development server
-let PREFIX_API_URL = 'https://api-prod.asani.net';
+let PREFIX_API_URL = 'https://api-staging.asani.net';
 // let PREFIX_API_URL = 'http://localhost:8080';
 
 if (process.env.NODE_ENV === 'production') {
